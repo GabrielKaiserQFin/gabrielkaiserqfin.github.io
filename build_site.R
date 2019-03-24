c04530745a0291ef7c487acb7e2c6421312c012e
Gabriel KAISER <gabriel.kaiser@uni.lu>
2019-03-24 00:24:53 +0000

    My first website commit. The begining of greatness

---
+++ build_site.R
@@ -0,0 +1,10 @@
+#Set our working directory. 
+#This helps avoid confusion if our working directory is 
+#not our site because of other projects we were 
+#working on at the time. 
+setwd(paste0("C:/Users/", 
+             Sys.info()["login"], 
+             "/Google Drive/Blog/Website/"))
+
+#render your sweet site. 
+rmarkdown::render_site()
