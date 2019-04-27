66a7410ed80196fee30acd527361125e2fb2a4bb
Gabriel KAISER <gabriel.kaiser@uni.lu>
2019-04-27 14:35:19 +0100

    update

---
+++ js/brownian_motion.js
@@ -0,0 +1,13 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#brownian_motion').scianimator({
+	    'images': ['images/brownian_motion1.png', 'images/brownian_motion2.png', 'images/brownian_motion3.png', 'images/brownian_motion4.png', 'images/brownian_motion5.png', 'images/brownian_motion6.png', 'images/brownian_motion7.png', 'images/brownian_motion8.png', 'images/brownian_motion9.png', 'images/brownian_motion10.png', 'images/brownian_motion11.png', 'images/brownian_motion12.png', 'images/brownian_motion13.png', 'images/brownian_motion14.png', 'images/brownian_motion15.png', 'images/brownian_motion16.png', 'images/brownian_motion17.png', 'images/brownian_motion18.png', 'images/brownian_motion19.png', 'images/brownian_motion20.png', 'images/brownian_motion21.png', 'images/brownian_motion22.png', 'images/brownian_motion23.png', 'images/brownian_motion24.png', 'images/brownian_motion25.png', 'images/brownian_motion26.png', 'images/brownian_motion27.png', 'images/brownian_motion28.png', 'images/brownian_motion29.png', 'images/brownian_motion30.png', 'images/brownian_motion31.png', 'images/brownian_motion32.png', 'images/brownian_motion33.png', 'images/brownian_motion34.png', 'images/brownian_motion35.png', 'images/brownian_motion36.png', 'images/brownian_motion37.png', 'images/brownian_motion38.png', 'images/brownian_motion39.png', 'images/brownian_motion40.png'],
+	    'width': 480,
+	    'delay': 50,
+	    'loopMode': 'loop',
+ 'controls': ['first', 'previous', 'play', 'next', 'last', 'loop', 'speed']
+	});
+	$('#brownian_motion').scianimator('play');
+    });
+})(jQuery);
