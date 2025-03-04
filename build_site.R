3fb1048b63e5d852b1d9c99e258ffd961881a2ed
unknown <quantresearch.gk@gmail.com>
2025-03-04 19:42:44 +0100

    stay tuned

--- build_site.R
+++ build_site.R
@@ -2,9 +2,9 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd(paste0("C:/Users/", 
-             Sys.info()["login"], 
-             "/Google Drive/Blog/Website"))
+# setwd(paste0("C:/Users/", 
+#              Sys.info()["login"], 
+#              "/Google Drive/Blog/Website"))
 
 #render your sweet site. 
 rmarkdown::render_site()
